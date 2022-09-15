938aebb3ac14c96c22ca12d934262085f184f328
up210356 <up210356@alumnos.upa.edu.mx>
2022-09-15 13:24:59 -0500

    TiposDatos2

--- U1/TiposDatos.cpp
+++ U1/TiposDatos.cpp
@@ -3,7 +3,7 @@
 //Fecha: 15/9/2022
 //Descripción: Muestra los diferentes tipos de datos en c++
 
-#include <iostream>
+#include <iostream>|    
 #include <stdio.h>
 using namespace stdf;
 
@@ -18,20 +18,6 @@ int main()
     cout << "El número entero es: "<< entero << endl;
     cout << "El tamaño del entero es: " <sizeof(entero) < "bytes" <<endl;
 
-    /*
-    cout << "El número flotante es: " << flotante << endl;
-    cout << "El tamaño del flotante es: " << sizeof(flotante)<< "bytes" << endl;
-    cout << "El número double es: " << grande << endl;
-    cout << "El tamaño del double es: " << sizeof(grande)<< "bytes" << endl;
-    cout << "El caracter char es: " << caracter << endl;
-    cout << "El tamaño del char es: " << sizeof(caracter)<< "byte" << endl;
-    
-    cout << "=================" << endl;
-
-    printf("Usando la función printf\n");
-    printf("El número entero es: %i \n", entero);
-    printf("El número flotante es: %3.1f \n", flotante);
-    */
 
    getchar();
    return 0;
