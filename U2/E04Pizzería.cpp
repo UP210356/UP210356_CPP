2c1cf26ab0e5c78b02922f81d9956905cc7c608f
UP210356 <up210356@alumnos.upa.edu.mx>
2022-10-05 11:51:20 -0500

    Primer Pizza

---
+++ U2/E04Pizzería.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char 
+    cout << "Dime qué tipo de pizza gustas \n";
+    cout << "Escribe v si es vegetariana y n si no es vegetariana \n";
+    cin >> 
+
+}
