5c08fc095349351656ed08e87f2e49c27476d40f
UP210356 <113048706+UP210356@users.noreply.github.com>
2022-10-10 19:12:27 -0500

    Update and rename E05Temperatura.cpp to 05Temperatura.cpp

---
+++ U2/05Temperatura.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float t, s, p, max, min;
+    for (int i=0; i<6; i++)
+    {
+        cout<<"Ingrese la temperatura: \n";
+        cin>>t;
+        s=s+t;
+        if (min>t)
+        {
+            min=t;
+        }
+        if (max<t)
+        {
+            max=t;
+        }
+    }
+    p=t/6
+    cout<<"El promedio de las temperaturas es: "<< p <<" grados \n";
+    cout<<"La temperatura más baja es: "<< min <<" grados \n";
+    cout<<"La temperatura más alta es: "<< max <<" grados \n";
+}
