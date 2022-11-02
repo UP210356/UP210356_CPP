0bbec442acfc5e1668c068c00362490939181788
UP210356 <113048706+UP210356@users.noreply.github.com>
2022-11-01 20:51:47 -0600

    Update 07Conversión.cpp

--- U2/07Conversión.cpp
+++ U2/07Conversión.cpp
@@ -3,14 +3,30 @@ using namespace std;
 
 int main()
 {
-    string bin = "10110";
-    int longitud = bin.length();
-    int num, exp;
-    for (int i = longitud; i > 0; i--)
+    int numro;
+    string res;
+    cout << "Depositar un número en decimal: ";
+    cin >> numero; 
+    do
     {
-        exp=
-        num = i
-    }
+        if (numero > 0)
+        {
+            while (numero != 0)
+            {
+                res = (numero % 2 == 0) ? "0" + res : "1" + res;
 
+                numero /= 2;
+            }
+    cout << "E lnúmero en binario es: " << res << endl;
+        }
+        else if (numero == 0)
+        {
+        cout << "Por favor introducir un número mayor a 0" <<endl;
+        }
 
+        // Line break
+        cout << "\n";
+    } while (numero < 0);
+
+    return 0;
 }
