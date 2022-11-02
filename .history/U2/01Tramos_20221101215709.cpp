ed567fc48080156d2d04b0b0a95fd8cbb197114a
unknown <up210360@alumnos.upa.edu.mx>
2022-11-01 21:58:24 -0600

    Listo

---
+++ .history/U2/01Tramos_20221101215709.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int renta, desc, impuesto;
+    cout << "Ingresa tu renta anual" << endl;
+    cin >> renta;
+
+    if (renta < 10000)
+    {
+        cout << "Tu impuesto es del 5%";
+        desc = 5;
+    }
+    else if (renta <= 20000)
+    {
+        cout << "Tu impuesto es del 10%";
+        desc = 10;
+    }
+    else if (renta <= 35000)
+    {
+        cout << "Tu impuesto es del 15%";
+        desc = 15;
+    }
+    else if (renta <= 60000)
+    {
+        cout << "Tu impuesto es del 30%";
+        desc = 30;
+    }
+    else if (renta > 60000)
+    {
+        cout << "Tu impuesto es del 45%";
+        desc = 45;
+    }
+    impuesto = renta * desc / 100;
+    cout << " \n Debes de pagar: $ " << impuesto << endl;
+    
+    return 0;
+}
